1d5be6970807fa7f83fa052cdb46e2474ff57ddc
ver09934 <ianvernooy@gmail.com>
2018-07-04 19:55:28 -0400

    Add RPi.GPIO output test

---
+++ tests/blink.py
@@ -0,0 +1,12 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.OUT)
+
+while True:
+	GPIO.output(4, GPIO.HIGH)
+	time.sleep(1)
+	GPIO.output(4, GPIO.LOW)
+	time.sleep(1)
+
