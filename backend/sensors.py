62388fa0d65794160de260e61f1ca8089caa1a91
ver09934 <ianvernooy@gmail.com>
2018-07-04 23:41:36 -0400

    Work on sensor script

--- backend/sensors.py
+++ backend/sensors.py
@@ -1,5 +1,3 @@
-# Note: To make a python script executable: put "#!/usr/bin/env python" (no quotes) at the top, and of course, run chmod +x filename.py
-
 '''
 Structure:
 - read the PIR sensor
@@ -15,15 +13,60 @@ PIR Condition:
 - PIR has been high for at least 1 second (assuming time the output signal stays high when object is detected is only when there is active motion)
 - TODO: Improve this filtering system
 
-- If using external battery, create a separate script to run at boot that monitors battery voltage and creates alert (text/email?) on low battery using a voltage sensor on a gpio pin
-
-- Take still shot w/ camera in bash: raspistill -o cam.jpg
-
 PINOUT:
 - PIR Sensor connected to GND, +5V, and Pin (TODO) (regulates 5V power to a 3.3V logic level for the signal)
 - Barometric pressure sensor connected to (TODO)
 - Raspi camera module connected to the camera connector slot
 
+Notes:
+- To make a python script executable: put "#!/usr/bin/env python" (no quotes) at the top, and of course, run chmod +x filename.py
+- Take still shot w/ camera in bash: raspistill -o cam.jpg
+- If using external battery, create a separate script to run at boot that monitors battery voltage and creates alert (text/email?) on low battery using a voltage sensor on a gpio pin
+
+TODO:
+- Baro reading with I2C
+- Check at end of loop: If ~/.sensors-running contains 0 instead of 1 (writes 1 at startup), then break main loop and run GPIO.cleanup()
+
 '''
+
+import RPi.GPIO as GPIO
+import time
+import subprocess
+
+# Set initial sensor state
+state=False
+if GPIO.input(17):
+	state=True
+
+# TODO: Figure out how to gracefully exit this loop externally to un 
+while True:
+
+	# If state changes from low to high
+	if state == False and GPIO.input(17):
+    
+		state=True
+		print("State changed to high")
+
+		cmd = "raspistill -o ~/Pictures/test" + str(picCount) + ".jpg" # TODO: Can this be run as a background task so we don't have to wait for it?
+		print(cmd)
+
+		# subprocess.Popen(cmd)
+
+		# subprocess.check_output(['bash','-c', cmd])
+
+		subprocess.check_output(cmd, shell=True)
+
+		picCount += 1
+
+	# If state changes from high to low
+	if state == True and (not GPIO.input(17)):
+    
+		state=False
+		print("State changed to low")
+
+	# Looptime (time between PIR reads) assuming no sensor activity
+	time.sleep(0.1)
+
+
 
 
