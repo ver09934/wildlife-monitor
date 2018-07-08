4d4158822ec2e7dbc8d9be6076bc688b68de1c0c
ver09934 <ianvernooy@gmail.com>
2018-07-08 09:58:06 -0400

    Create table of mp4 and json files

--- site/index.php
+++ site/index.php
@@ -13,41 +13,71 @@
       <h1>Wildlife Monitor Homepage</h1>
       <h2>Recent Activity</h2>
       
+      <p>The entries at the top are the most recent. To see the time a video was recorded, see the metadata file.</p>
+      
       <table>
         <tr>
-          <th>Time</th>
+          <th>Number</th>
           <th>Video</th> 
-          <th>Temperature</th>
+          <th>Metadata</th>
         </tr>
-        <tr>
-          <td>content1</td>
-          <td>content2</td> 
-          <td>content3</td>
-        </tr>
-        <tr>
-          <td>content4</td>
-          <td>content5</td> 
-          <td>content6</td>
-        </tr>
+      
+        <?php
+        
+          // echo '<pre><a href="' . $path . $item . '">' . $item . '</a></pre>' . "\n";
+          // echo pathinfo($item, PATHINFO_EXTENSION);
+
+          $path = 'data/';
+          $files = scandir($path);
+          $files = array_diff($files, array('.', '..'));
+          
+          $mp4files = array();
+          $jsonfiles = array();
+          
+          foreach($files as $item) {
+            
+            $ext = pathinfo($item, PATHINFO_EXTENSION);
+            
+            if ($ext == "mp4") {
+              array_push($mp4files, $item);
+            } else {
+              array_push($jsonfiles, $item);
+            }
+                      
+          }
+          
+          if (count($jsonfiles) > count($mp4files)) {
+                      
+            $diff = count($jsonfiles) - count($mp4files);
+            
+            for ($x = 0; $x < $diff; $x++) {
+                array_push($mp4files, "File currently unavailable");
+            }
+            
+          }
+          
+          $mp4files = array_reverse($mp4files);
+          $jsonfiles = array_reverse($jsonfiles);
+                    
+          for ($x = 0; $x < count($jsonfiles); $x++) {
+            echo '<tr>';
+            
+            echo '<td>' . strval($x + 1) . '</td>';
+            
+            if ($mp4files[$x] == "File currently unavailable") {
+              echo '<td>' . '<pre>' . $mp4files[$x] . '</pre>' . '</td>';
+            } else {
+              echo '<td>' . '<pre>' . '<a href="' . $path . $mp4files[$x] . '">' . $mp4files[$x] . '</a>' . '</pre>' . '</td>';
+            }
+            
+            echo '<td>' . '<pre>' . '<a href="' . $path . $jsonfiles[$x] . '">' . $jsonfiles[$x] . '</a>' . '</pre>' . '</td>';
+                      
+            echo '</tr>';
+          }
+          
+        ?>
+      
       </table>
-      
-      <h2>Debug</h2>
-      <p>This is placeholder text.</p>
-      
-      <?php
-      
-        $path = 'data/';
-        
-        $files = array_diff(scandir($path), array('.', '..'));
-        
-        foreach($files as $item) {
-          // echo $item['filename'];
-          // echo $item['filepath'];
-          echo '<pre><a href="' . $path . $item . '">' . $path . $item . '</a></pre>' . "\n";
-          //echo '<pre>'; var_dump($item);
-        }
-        
-      ?>
 
     </div>
   </body>
