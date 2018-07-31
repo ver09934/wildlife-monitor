f885803343b714c898e9e912e183a30e01a4d02f
ver09934 <ianvernooy@gmail.com>
2018-07-31 09:39:25 -0400

    Work on frontend

--- new-frontend/testunit/chart.js
+++ new-frontend/testunit/chart.js
@@ -4,44 +4,61 @@ xhttp.onreadystatechange = function() {
         readData(xhttp);
     }
 };
-xhttp.open("GET","data/current_log.xml", true);
+xhttp.open("GET","data/datalogs/current_log-2018-07-31.xml", true);
 xhttp.send();
-
-function drawLineColors(xml) {
-    
-    var data = new google.visualization.DataTable();
-    data.addColumn('number','x');
-    data.addColumn('number','Temperature');
-    data.addColumn('number','Pressure');
-
-    var xmlDoc = xml.responseXML;
-    var rows = xmlDoc.getElementsByTagName("row");
-
-    for(let i = 0; i < rows.length; i++) {
-        data.addRow([i,
-            Number(rows[i].getElementsByTagName("temp")[0].childNodes[0].nodeValue),
-            Number(rows[i].getElementsByTagName("pres")[0].childNodes[0].nodeValue)]);
-    }
-
-    //var line = file.readln().split(";");
-    //data.addRow([count++,line[1],line[2]]);
-
-    var option = {
-        hAxis: {
-            title: 'Time (Needs to show actual time...)'
-        },
-        vAxis: {
-            title: 'Temperature / Pressure'
-        },
-        colors: ['#a52714', '#097138']
-    };
-
-    var chart = new google.visualization.LineChart(document.getElementById('baro_chart'));
-    chart.draw(data,option);
-}
 
 function readData(xml) {
     var xmlDoc = xml.responseXML;
     google.charts.load('current', {packages: ['corechart', 'line']});
     google.charts.setOnLoadCallback(function() {drawLineColors(xml);});
 }
+
+function drawLineColors(xml) {
+    
+    var tempData = new google.visualization.DataTable();
+    var presData = new google.visualization.DataTable();
+    tempData.addColumn('number','x');
+    tempData.addColumn('number','Temperature');
+    presData.addColumn('number','x');
+    presData.addColumn('number','Pressure');
+
+    var xmlDoc = xml.responseXML;
+    var rows = xmlDoc.getElementsByTagName("row");
+
+    for(let i = 0; i < rows.length; i++) {
+        tempData.addRow([i, Number(rows[i].getElementsByTagName("temperature")[0].childNodes[0].nodeValue)]);
+        presData.addRow([i, Number(rows[i].getElementsByTagName("pressure")[0].childNodes[0].nodeValue)]);
+    }
+
+    var tempOptions = {
+        'title':'Temperature over Time',
+        legend: 'none',
+        hAxis: {
+            title: 'Time'
+        },
+        vAxis: {
+            title: 'Temperature (\xB0C)'
+        },
+        colors: ['#a52714']
+    };
+    
+    var presOptions = {
+        'title':'Pressure over Time',
+        legend: 'none',
+        hAxis: {
+            title: 'Time'
+        },
+        vAxis: {
+            title: 'Pressure (Pa)'
+        },
+        colors: ['#097138']
+    };
+
+    var tempChart = new google.visualization.LineChart(document.getElementById('temp_chart'));
+    var presChart = new google.visualization.LineChart(document.getElementById('pres_chart'));
+    
+    tempChart.draw(tempData, tempOptions);
+    presChart.draw(presData, presOptions);
+}
+
+
--- new-frontend/testunit/index.php
+++ new-frontend/testunit/index.php
@@ -18,23 +18,91 @@
       
       <h3>Temperature/Barometric Pressure</h3>
       
-      <div id="baro_chart" style="width: 500px; height: 300px"></div>
+      <div id="temp_chart" style="width: 600px; height: 400px" class="datagraph"></div>
+      <div id="pres_chart" style="width: 500px; height: 400px" class="datagraph"></div>
       
       <h2>Livestream</h2>
+      
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/NpEaa2P7qZI" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
       
       <h2>Recorded Videos</h2>
       
       <table>
         <tr>
-          <th>Video Number</th>
-          <th>Video Link</th> 
-          <th>Data from Video Start</th> 
+          <th>Number</th>
+          <th>Video</th> 
+          <th>Time</th>
+          <th>Temperature</th>
+          <th>Pressure</th>
+          <th>Metadata File</th>
         </tr>
-        <tr>
-          <td>1</td>
-          <td><a href="">TODO (php)</a></td>
-          <td><a href="">TODO (php)</a></td>
-        </tr>
+      
+        <?php
+
+          $path = 'data/videos/';
+          $files = scandir($path);
+          $files = array_diff($files, array('.', '..'));
+          
+          $mp4files = array();
+          $xmlfiles = array();
+          
+          foreach($files as $item) {
+            
+            $ext = pathinfo($item, PATHINFO_EXTENSION);
+            
+            if ($ext == "mp4") {
+              array_push($mp4files, $item);
+            } elseif ($ext == "xml") {
+              array_push($xmlfiles, $item);
+            }
+                      
+          }
+          
+          if (count($xmlfiles) > count($mp4files)) {
+                      
+            $diff = count($xmlfiles) - count($mp4files);
+            
+            for ($x = 0; $x < $diff; $x++) {
+                array_push($mp4files, "File currently unavailable");
+            }
+            
+          }
+          
+          $mp4files = array_reverse($mp4files);
+          $xmlfiles = array_reverse($xmlfiles);
+                    
+          for ($x = 0; $x < count($xmlfiles); $x++) {
+            echo '<tr>';
+            
+            echo '<td>' . strval(count($xmlfiles) - $x) . '</td>';
+            
+            if ($mp4files[$x] == "File currently unavailable") {
+              echo '<td>' . '<pre>' . $mp4files[$x] . '</pre>' . '</td>';
+            }
+            else {
+              echo '<td>' . '<pre>' . '<a href="' . $path . $mp4files[$x] . '">' . $mp4files[$x] . '</a>' . '</pre>' . '</td>';
+            }
+            
+            // Server needs php-xml to be installed
+            if (file_exists($path . $xmlfiles[$x])) {
+                $xml = simplexml_load_file($path . $xmlfiles[$x]);
+                echo '<td>' . '<pre>' . $xml->row[0]->time . '</pre>' . '</td>';
+                echo '<td>' . '<pre>' . $xml->row[0]->temperature . ' &deg;C' . '</pre>' . '</td>';
+                echo '<td>' . '<pre>' . $xml->row[0]->pressure . ' Pa' . '</pre>' . '</td>';
+                
+            } else {
+                echo '<td>' . '<pre>' . 'File not available' . '</pre>' . '</td>';
+                echo '<td>' . '<pre>' . 'File not available' . '</pre>' . '</td>';
+                echo '<td>' . '<pre>' . 'File not available' . '</pre>' . '</td>';
+            }
+                               
+            echo '<td>' . '<pre>' . '<a href="' . $path . $xmlfiles[$x] . '">' . $xmlfiles[$x] . '</a>' . '</pre>' . '</td>';
+            
+            echo '</tr>';
+          }
+          
+        ?>
+      
       </table>
 
     </div>
