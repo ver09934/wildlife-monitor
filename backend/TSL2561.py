5d7a680703eb52da5294d87eba451292ae76685c
ver09934 <ianvernooy@gmail.com>
2019-06-13 13:41:10 -0400

    Copy sensor scripts from power-test-v2 branch

---
+++ backend/TSL2561.py
@@ -0,0 +1,45 @@
+import time
+try:
+    from smbus import SMBus
+except ImportError:
+    from smbus2 import SMBus
+
+# Get I2C bus
+bus = smbus.SMBus(1)
+
+def getData():
+
+    # TSL2561 address, 0x39(57)
+    # Select control register, 0x00(00) with command register, 0x80(128)
+    #		0x03(03)	Power ON mode
+    bus.write_byte_data(0x39, 0x00 | 0x80, 0x03)
+    # TSL2561 address, 0x39(57)
+    # Select timing register, 0x01(01) with command register, 0x80(128)
+    #		0x02(02)	Nominal integration time = 402ms
+    bus.write_byte_data(0x39, 0x01 | 0x80, 0x02)
+
+    time.sleep(0.5)
+
+    # Read data back from 0x0C(12) with command register, 0x80(128), 2 bytes
+    # ch0 LSB, ch0 MSB
+    data = bus.read_i2c_block_data(0x39, 0x0C | 0x80, 2)
+
+    # Read data back from 0x0E(14) with command register, 0x80(128), 2 bytes
+    # ch1 LSB, ch1 MSB
+    data1 = bus.read_i2c_block_data(0x39, 0x0E | 0x80, 2)
+
+    # Convert the data
+    ch0 = data[1] * 256 + data[0]
+    ch1 = data1[1] * 256 + data1[0]
+
+    return [ch0, ch1]
+
+def main():
+    data = getData()    
+    print("Full Spectrum (IR + Visible): " + str(data[0] + " lux")
+    print("Infrared Value: " + str(data[1]) + " lux")
+    print("Visible Value: " + str(data[0] - data[1]) + " lux")
+
+if __name__ == '__main__':
+    main()
+
